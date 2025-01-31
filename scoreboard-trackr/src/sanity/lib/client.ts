32efdc9ea15f29eba45e85a390f100292b85f21d
pycomet <alfredemmanuelinyang@gmail.com>
2025-01-31 22:25:41 +0100

    add ui library

--- scoreboard-trackr/src/sanity/lib/client.ts
+++ scoreboard-trackr/src/sanity/lib/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from 'next-sanity';
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+  projectId: "h8qmltcs",
+  dataset: "production",
+  apiVersion: "2024-07-11",
   useCdn: true,
 });
